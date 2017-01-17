3cd108d2292ec3d25773a761bcd3d952d24c1122
Fanat1k <nso.fanat1k@gmail.com>
2017-01-17 14:24:17 +0700

    Переработан модуль отправки ccn комманд

--- modules/cocon_interface.py
+++ modules/cocon_interface.py
@@ -16,12 +16,16 @@ class coconInterface:
         self.Ip = str(test_var["%%SERV_IP%%"])
         self.Port = int(8023)
         self.sshChannel = None
+        self.sshClient = None
         self.ConnectionStatus = True
         self.coconQueue = queue.Queue()
         self.eventForStop = None
         self.myThread = None
         self.attempt = 0
         self.ShowCoConOutput = show_cocon_output
+        self.read_buff = b""
+        self.buff_size = 1024
+        self.data = b""
 
     def flush_queue(self):
         logger.info("Flashing CCN Queue. Num of tasks: %s", self.coconQueue.qsize())
@@ -37,16 +41,21 @@ class coconInterface:
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             client.connect(hostname = self.Ip, username = self.Login, password = self.Password, port = self.Port, timeout=10, banner_timeout = 10, look_for_keys=False, allow_agent=False)
+            self.sshClient = client
         except:
             logger.warning("Exception on ssh connect.")
             if client:
                 logger.debug("Close ssh connect")
                 client.close()
             logger.debug("Set ssh channel to disable.")
-            self.sshChannel = False
+            self.Client = False
 
+        if self.sshClient:
+            try:
+                self.sshChannel = self.sshClient.invoke_shell()
+            except:
+                self.sshChannel = False
         if self.sshChannel != False:
-            self.sshChannel = client
             return True
         else:
             logger.debug("Exit from get_channel method.")
@@ -57,29 +66,45 @@ class coconInterface:
         if self.get_channel():
             logger.info("---> Command: %s",command)
             try:
-                stdin, stdout, stderr = self.sshChannel.exec_command(command, get_pty=True, bufsize=-1, timeout = 10)
+                self.sshChannel.sendall(command)
             except:
-                logger.warning("Exception on exec ssh command! Close ssh connection")
-                return False
+                 logger.warning("Exception on exec ssh command! Close ssh connection")
+                 return False
+            #ждём когда отпустит ccn
+            logger.debug("Waiting for recv_exit_status.")
+            self.sshChannel.recv_exit_status()
+            logger.debug("Recv ccn output.")
             #Сохраняем вывод
-            data = stdout.read() + stderr.read()
-            #Закрываем ssh соединение
-            self.sshChannel.close()
-            #Устанавливаем sshChannel в None
-            self.sshChannel = None
+            while True:
+                self.read_buff = self.sshChannel.recv(self.buff_size)
+                if len(self.read_buff) == 0:
+                    break
+                else:
+                    self.data += self.read_buff
             #Даём кокону очнуться            
-            time.sleep(0.5)
+            time.sleep(0.1)
             if self.ShowCoConOutput:
                 logger.info("CoconOutput: ")
-                logger.info(data.decode("utf-8", "strict"))
+                logger.info(self.data.decode("utf-8", "strict"))
             #Возвращаем True
+            #Закрываем ssh соединение
+            logger.debug("Close ssh connection.")
+            self.close_connection()
             return True
         else:
             #Возвращаем False
             return False
 
+    def close_connection(self):
+        self.sshChannel.close()
+        self.sshClient.close()
+        self.sshChannel = None
+        self.sshClient = None
+        self.read_buff = b""
+        self.data = b""
 
 def ccn_command_handler(coconInt):
+    command = ""
     while True:
         #Если прищёл event на завершение треда
         #И при этом очередь пуста, то выходим
@@ -87,7 +112,7 @@ def ccn_command_handler(coconInt):
             logger.info("Stop event is set. I'm going down...{cocon thread}")
             break
         #Если очередь пустая, то делаем паузу. (чтобы не тратить ресурсы)
-        if coconInt.coconQueue.empty():
+        if coconInt.coconQueue.empty() and command == "":
             time.sleep(0.1)
             continue
         if not coconInt.ConnectionStatus:
@@ -106,8 +131,7 @@ def ccn_command_handler(coconInt):
             if coconInt.send_command(command):
                 #Если команда прошла успешно, то завершаем задачу. 
                 coconInt.coconQueue.task_done()
-                if coconInt.sshChannel:
-                    coconInt.sshChannel.close()
+                command = ""
                 coconInt.attempt = 0
             else:
                 coconInt.attempt += 1
@@ -116,19 +140,22 @@ def ccn_command_handler(coconInt):
                     coconInt.ConnectionStatus  = False
                     coconInt.coconQueue.task_done()
                 logger.info("CCN overload. Sleep before next attempt.")
-                time.sleep(random.randint(2, 10))
+                #time.sleep(random.randint(2, 10))
 
     
 def cocon_configure(CoconCommands,coconInt,test_var):
     CoconCommands = CoconCommands[0]
+    cmd_string = ""
     for CoconCommand in CoconCommands.values():
         #Пропускаем команду через словарь
         CoconCommand = builder.replace_key_value(CoconCommand, test_var)
         if CoconCommand:
-            #Если команда собралась без ошибок отправляем её в thread
-            coconInt.coconQueue.put(CoconCommand)
+            cmd_string += CoconCommand + "\n"
         else:
             return False
+    cmd_string += "exit\n"
+    #Если команда собралась без ошибок отправляем её в thread
+    coconInt.coconQueue.put(cmd_string)
     #Ждём пока thread разгребёт очередь
     coconInt.coconQueue.join()
     #Проверяем, что все команды были отправлены
