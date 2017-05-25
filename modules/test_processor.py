6e666a0faa26d0daad8279fce8e1b3ee4597f387
Fanat1k <nso.fanat1k@gmail.com>
2017-05-25 17:33:51 +0700

    fix format of ua info

--- modules/test_processor.py
+++ modules/test_processor.py
@@ -233,6 +233,7 @@ class TestProcessor():
             logging.info("PortInfo for UA: %s", ua.Name)
             logging.info("---| UA Type:     %s", str(ua.Type))
             if ua.Type == "User":
+                logging.info("---| UA Number:   %s", str(ua.UserObject.Number))
                 logging.info("---| UA Port:     %s", str(ua.UserObject.Port))
                 logging.info("---| UA RtpPort:  %s", str(ua.UserObject.RtpPort))
             elif ua.Type == "Trunk":
